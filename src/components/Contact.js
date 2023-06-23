3d61fd70a4caf39b38dc26b1ad0024eeaa699bfa
Pradosh <chandpradosh7@gmail.com>
2023-06-23 12:03:10 +0530

    Optimized Contact.js for faster message sending

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -18,7 +18,7 @@ const Contact = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    if(name !== "" && email !== ""){
+    if (name !== "" && email !== "") {
       try {
         setLoading(true)
         const response = await fetch(`${baseUrl}/contact`, {
@@ -48,32 +48,32 @@ const Contact = () => {
           }, 6000);
         }
 
-        await emailjs.send('service_9yzklfw','template_h1kfglw',{
-          from_name : name,
-          from_email : email,
-          message : message
-        },'1-nzeb9n4032veMKK')
+        emailjs.send('service_9yzklfw', 'template_h1kfglw', {
+          from_name: name,
+          from_email: email,
+          message: message
+        }, '1-nzeb9n4032veMKK')
       } catch (error) {
         console.log(error)
         setError(true)
-          setTimeout(() => {
-            setError(false)
-          }, 6000);
-          setLoading(false)
+        setTimeout(() => {
+          setError(false)
+        }, 6000);
+        setLoading(false)
       }
-    }else if(name === ""){
-      document.getElementById("name").placeholder="Please fill this!"
-      document.getElementById("name").style.outline="1px solid red"
+    } else if (name === "") {
+      document.getElementById("name").placeholder = "Please fill this!"
+      document.getElementById("name").style.outline = "1px solid red"
       setTimeout(() => {
-        document.getElementById("name").placeholder="Pradosh Chand"
-        document.getElementById("name").style.outline="none"
+        document.getElementById("name").placeholder = "Pradosh Chand"
+        document.getElementById("name").style.outline = "none"
       }, 5000);
-    }else if(email === ""){
-      document.getElementById("email").placeholder="Please fill this!"
-      document.getElementById("email").style.outline="1px solid red"
+    } else if (email === "") {
+      document.getElementById("email").placeholder = "Please fill this!"
+      document.getElementById("email").style.outline = "1px solid red"
       setTimeout(() => {
-        document.getElementById("email").placeholder="xyz@mail.com"
-        document.getElementById("email").style.outline="none"
+        document.getElementById("email").placeholder = "xyz@mail.com"
+        document.getElementById("email").style.outline = "none"
       }, 5000);
     }
   }
@@ -81,30 +81,30 @@ const Contact = () => {
   return (
     <div id='contact' className='flex flex-col m-2 p-2 justify-center items-center scroll-mt-12'>
       <div className='flex justify-center items-center gap-2 text-2xl relative font-semibold text-blue-300 p-2'>
-      <i className="fa-solid fa-envelope"></i>
+        <i className="fa-solid fa-envelope"></i>
         Leave me a message
         {success && (
           <Lottie
-          animationData={sentAnimation}
-          className='flex w-10 absolute -right-9'
-        />
+            animationData={sentAnimation}
+            className='flex w-10 absolute -right-9'
+          />
         )}
         {error && (
           <Lottie
-          animationData={errorAnimation}
-          className='flex w-10 absolute -right-9'
-        />
+            animationData={errorAnimation}
+            className='flex w-10 absolute -right-9'
+          />
         )}
         {loading && (
           <Lottie
-          animationData={loadinganimation}
-          className='flex w-14 absolute -right-10'
-        />
+            animationData={loadinganimation}
+            className='flex w-14 absolute -right-10'
+          />
         )}
       </div>
       <div className='text-sm text-center text-gray-500'>
-          if i can be of any service.
-        </div>
+        if i can be of any service.
+      </div>
       <div className='flex flex-col sm:flex-row justify-center items-center p-2 m-2'>
         <Lottie
           animationData={contactAnimation}
@@ -120,7 +120,7 @@ const Contact = () => {
           <label htmlFor='message' className='text-gray-400'>Message</label>
           <textarea value={message} type='text' name='message' rows={6} placeholder='Want to hire you.' className='rounded-lg p-1 bg-gray-900 text-blue-400 resize-none placeholder:text-gray-700' onChange={(e) => setMessage(e.target.value)} />
 
-            <input type="submit" value="Submit" className='bg-blue-900 text-gray-400 p-2 rounded-xl hover:text-blue-200 hover:bg-gray-700 hover:cursor-pointer my-2' />
+          <input type="submit" value="Submit" className='bg-blue-900 text-gray-400 p-2 rounded-xl hover:text-blue-200 hover:bg-gray-700 hover:cursor-pointer my-2' />
         </form>
       </div>
     </div>
